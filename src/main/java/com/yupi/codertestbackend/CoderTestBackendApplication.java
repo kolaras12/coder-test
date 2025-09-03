192d1e19c4413268c2e2f3f42b9b92830790b926
1-鱼皮 <592789970@qq.com>
2025-09-03 21:27:26 +0800

    后端 - 后端初始化
    1. 整合常用依赖
    2. 编写各种配置

---
+++ src/main/java/com/yupi/codertestbackend/CoderTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.codertestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoderTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yupi/codertestbackend/CoderTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.codertestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
